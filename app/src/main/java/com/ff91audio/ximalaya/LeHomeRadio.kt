fe90bdb059c6e78f3eb0e35dff497beb4da64cf6
Dong Xia <dong.xia@ff.com>
2018-11-19 15:32:51 +0800

    new project

---
+++ app/src/main/java/com/ff91audio/ximalaya/LeHomeRadio.kt
@@ -0,0 +1,12 @@
+package com.ff91audio.ximalaya
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeHomeRadio : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_le_home_radio)
+    }
+}
